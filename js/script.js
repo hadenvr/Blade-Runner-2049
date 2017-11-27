8b40d710dda8d367d6d73c3cb3adf305f7df56ac
ACE <acetip@aces-mbp.lab.lagcc>
2017-11-27 16:46:06 -0500

    process

---
+++ js/script.js
@@ -0,0 +1,15 @@
+fetch("https://api.giphy.com/v1/gifs/search?q=ryangosling&api_key=dc6zaTOxFJmzC&limit=5")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(response) {
+    console.log(response);
+  });
+
+  function showGifs(data){
+    for (var x = 0; x < data.length; x++) {
+      var img = document.createElement('img');
+      img.src = data[x].images.original.url;
+      document.body.appendChild(img);
+    }
+  }
